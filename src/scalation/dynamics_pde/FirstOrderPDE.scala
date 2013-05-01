5f7442979b6f4b2ff04b4b629514633d478c8003
Michael E. Cotterell <mepcotterell@gmail.com>
2013-05-01 18:13:35 -0400

    added new packages

---
+++ src/scalation/dynamics_pde/FirstOrderPDE.scala
@@ -0,0 +1,163 @@
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+/** @author  John Miller
+ *  @version 1.0
+ *  @date    Thu Mar 28 13:43:50 EDT 2013
+ *  @see     LICENSE (MIT style license file)
+ *  @see     www.ita.uni-heidelberg.de/~dullemond/lectures/.../Chapter_3.pdf
+ *  @see     www2.hawaii.edu/~norbert/CompPhys/chapter14.pdf
+ */
+
+package scalation.dynamics_pde
+
+import math.{abs, ceil, exp}
+
+import scalation.calculus.Calculus.FunctionS2S
+import scalation.linalgebra.{MatrixD, VectorD}
+import scalation.util.Error
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+/** This class is used to solve first order partial differential equations like
+ *  the Advection Equation.  Let u(x, t) = concentration in a fluid with velocity
+ *  v at position 0 <= x <= xm and time t > 0.  Numerically solve the
+ *  Advection Equation:      u_t + v(x, t) * u_x = 0
+ *  with initial conditions  u(x, 0) = ic(x)
+ *      boundary conditions  (u(0, t), u(xm, t)) = bc
+ *  @param v   the velocity field function v(x, t)
+ *  @param dt  delta t
+ *  @param dx  delta x
+ *  @param xm  the length of the column
+ *  @param ic  the initial conditions as a function of position x
+ *  @param bc  the boundary conditions as a 2-tuple for endpoints 0 and xm
+ */
+class FirstOrderPDE (v: (Double, Double) => Double, dt: Double, dx: Double, xm: Double,
+                     ic: FunctionS2S, bc: Tuple2 [Double, Double])
+      extends Error
+{
+    private val nx = (ceil ((xm) / dx)).toInt + 1            // number of x values in grid
+    private val r  = dt / dx                                 // multiplier in recurrence equation
+    private var u  = new VectorD (nx)                        // old concentration vectors
+    private var uu = new VectorD (nx)                        // new concentration vectors
+
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    /** Solve for the concentration of the column at time t, returning the vector of
+     *  concentration representing the concentration profile of column over its length.
+     *  This method uses an explicit finite difference technique to solve the PDE.
+     *  L-W is the Lax-Wendroff scheme which has second-order accuracy.
+     *  @see   math.nju.edu.cn/~qzh/numPDE.pdf
+     *  @param te  the time the solution is desired (t-end)
+     */
+    def solve (te: Double): VectorD =
+    {
+        var j1 = 0                                           // current time index
+        var j2 = 1                                           // next time index
+        val nt  = (ceil (te / dt)).toInt + 1                 // number of t values in grid
+        for (i <- 0 until nx) u(i) = ic (i*dx)               // apply initial conditions
+        u(0) = bc._1                                         // apply boundary conditions, left only
+
+        println ("at t =  " + 0.0 + ": \tu  = " + u)
+
+        for (j <- 1 until nt) {                              // iterative over t = j*dt
+            val t = j*dt                                     // current time t
+            for (i <- 1 until nx-1) {                        // iterative over x = i*dx
+                val x = i*dx                                 // current position x
+                val a = r * v(x, t)
+                                                             // explicit recurrence equation
+//              uu(i) = u(i) - a * (u(i) - u(i-1))
+//              uu(i) = .5 * ((1. - a) * u(i+1) + (1. + a) * u(i-1))
+//              uu(i) = u(i) - .5 * a * u(i+1) + .5 * a * u(i-1)
+                uu(i) = u(i) - .5 * a * (u(i+1) - u(i-1)) + .5 * a*a * (u(i+1) - 2.*u(i) + u(i-1))  // L-W
+
+            } // for
+
+            println ("at t = " + "%5.2f".format (t) + ": \tuu = " + uu)
+            uu(0)  = bc._1                                   // apply left boundary condition
+            var ut = uu; uu = u; u = ut                      // swap new and old references
+        } // for
+        u                                                    // return the concentration vector
+    } // solve
+
+} // FirstOrderPDE
+
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+/** This object is used to test the FirstOrderPDE class.
+ *  Numerically solve the Advection Equation:  du/dt + v(x, t) * du/dx = 0
+ */
+object FirstOrderPDETest extends App
+{
+    val dt = 1.                                              // delta t in sec
+    val dx = 1.                                              // delta x in cm
+    val xm = 100.                                            // length of column in cm
+
+    def ic (x: Double): Double = if (x < 30.) 1. else 0.     // initial conditions
+    val bc = (1., 0.)                                        // boundary conditions - only a left bc
+
+    def v (x: Double, t: Double): Double = 1.                // the velocity field
+
+    val pde = new FirstOrderPDE (v, dt, dx, xm, ic, bc)
+
+    println (" Explicit Finite Difference ------------------------------------------")
+    println ("solution = " + pde.solve (30.))           // solve for t = 1, 2, ... sec
+
+} // FirstOrderPDETest
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+/** This object is used to test the FirstOrderPDE class.
+ *  Numerically solve the Advection Equation:  du/dt + v(x, t) * du/dx = 0
+ */
+object FirstOrderPDETest2 extends App
+{
+    val dt = .2                                              // delta t in sec
+    val dx = .2                                              // delta x in cm
+    val xm = 3.                                              // length/height of column in cm
+    val d  = 0.5                                             // decay width
+    val x0 = 0.
+
+    def ic (x: Double): Double =                             // initial conditions
+    {
+        xm / (1. + exp ((x - x0) / d))                       // logistic function
+    } // ic
+
+    for (i <- 0 to 15) printf ("x = %4.1f, \t%6.3f\n", i*dx, ic (i*dx)) 
+
+    val bc = (ic (0.), 0.)                                   // boundary conditions - only a left bc
+
+    def v (x: Double, t: Double): Double = 1.                // the velocity field
+
+    val pde = new FirstOrderPDE (v, dt, dx, xm, ic, bc)
+
+    println (" Explicit Finite Difference ------------------------------------------")
+    println ("solution = " + pde.solve (4.))           // solve for t = 2., .4 ... sec
+
+} // FirstOrderPDETest2
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+/** This object is used to test the FirstOrderPDE class.
+ *  Numerically solve the Advection Equation:  du/dt + v(x, t) * du/dx = 0
+ *  @see www.public.asu.edu/~hhuang38/pde_slides_numerical.pdf
+ */
+object FirstOrderPDETest3 extends App
+{
+    val EPSILON = 1E-9                                       // a value close to zero
+
+    val dt = .1                                              // delta t in sec
+    val dx = .2                                              // delta x in cm
+    val xm = 2.                                              // length/height of column in cm
+
+    def ic (x: Double): Double =                             // initial conditions
+    {
+        if (abs (x - .8) < EPSILON) 1. else 0.
+    } // ic
+
+    val bc = (ic (0.), 0.)                                   // boundary conditions - only a left bc
+
+    def v (x: Double, t: Double): Double = -1.               // the velocity field
+
+    val pde = new FirstOrderPDE (v, dt, dx, xm, ic, bc)
+
+    println (" Explicit Finite Difference ------------------------------------------")
+    println ("solution = " + pde.solve (2.))           // solve for t = .1, .2, ... sec
+
+} // FirstOrderPDETest3
+
---
+++ src/scalation/dynamics_pde/ParabolicPDE.scala
@@ -0,0 +1,154 @@
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+/** @author  John Miller
+ *  @version 1.0
+ *  @date    Thu Mar  7 14:20:25 EST 2013
+ *  @see     LICENSE (MIT style license file)
+ *  @see     gwu.geverstine.com/pdenum.pdf
+ */
+
+package scalation.dynamics_pde
+
+import math.ceil
+
+import scalation.calculus.Calculus.FunctionS2S
+import scalation.linalgebra.{MatrixD, VectorD}
+import scalation.util.Error
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+/** This class is used to solve parabolic partial differential equations like
+ *  the Heat Equation.  Let u(x, t) = temperature of a rod at position 0 <= x <= xm
+ *  and time t > 0.  Numerically solve the
+ *  Heat Equation:           u_t = k * u_xx
+ *  with initial conditions  u(x, 0) = ic(x)
+ *      boundary conditions  (u(0, t), u(xm, t)) = bc
+ *  @param k   the thermal conductivity
+ *  @param dt  delta t
+ *  @param dx  delta x
+ *  @param xm  the length of the rod
+ *  @param ic  the initial conditions as a function of position x
+ *  @param bc  the boundary conditions as a 2-tuple for endpoints 0 and xm
+ */
+class ParabolicPDE (k: Double, dt: Double, dx: Double, xm: Double,
+                    ic: FunctionS2S, bc: Tuple2 [Double, Double])
+      extends Error
+{
+    private val nx = (ceil ((xm) / dx)).toInt + 1            // number of x values in grid
+    private val r  = k * dt / (dx * dx)                      // multiplier in recurrence equation
+
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    /** Solve for the temperature of the rod at time t, returning the vector of
+     *  temperatures representing the temperature profile of the rod over its length.
+     *  This method uses an explicit finite difference technique to solve the PDE.
+     *  @param t  the time the solution is desired
+     */
+    def solve (t: Double): VectorD =
+    {
+        if (r >= .5) flaw ("solve", "multiplier r = " + r + " must be < .5 for stability")
+
+        val u  = new MatrixD (nx, 2)                         // old/new temperatures as column vectors
+        var j1 = 0                                           // current time index
+        var j2 = 1                                           // next time index
+        val nt  = (ceil (t / dt)).toInt + 1                  // number of t values in grid
+        for (i <- 1 until nx-2) u(i, 0) = ic (i*dx)          // apply initial conditions
+        u(0, 0) = bc._1; u(nx-1, 0) = bc._2                  // apply boundary conditions
+        u(0, 1) = bc._1; u(nx-1, 1) = bc._2                  // apply boundary conditions (to both)
+
+        println ("at t = " + 0. + ": \tu = " + u.col(j1))
+
+        for (j <- 1 until nt) {                              // iterative over t = j*dt
+            for (i <- 1 until nx-1) {                        // iterative over x = i*dx
+                                                             // explicit recurrence equation
+                u(i, j2) = u(i, j1) + r * (u(i-1, j1) - 2.*u(i, j1) + u(i+1, j1))
+
+            } // for
+            println ("at t = " + j*dt + ": \tu = " + u.col(j2))
+            j2 = j1; j1 = (j1 + 1) % 2                       // toggle indices (j2 <-> j1)
+        } // for
+        u.col(j1)                                            // return solution column vector at j1
+    } // solve
+
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    /** Solve for the temperature of the rod at time t, returning the vector of
+     *  temperatures representing the temperature profile of the rod over its length.
+     *  This method uses the implicit Crank-Nicolson technique to solve the PDE,
+     *  which provides greater stability and accuracy.
+     *  Implicit recurrence equation:
+     *      -r*u(i-1, j2) + 2.*(1.+r)*u(i, j2) - r*u(i+1, j2) =
+     *       r*u(i-1, j1) + 2.*(1.-r)*u(i, j1) + r*u(i+1, j1)
+     *  This equation is solved simultaneously:  solve for u in mat * u = vec
+     *  @see people.sc.fsu.edu/~jpeterson/5-CrankNicolson.pdf
+     *  @param t  the time the solution is desired
+     */
+    def solveCN (t: Double): VectorD =
+    {
+        val u  = new VectorD (nx)                            // temperatures as a vector
+        val nt = (ceil (t / dt)).toInt + 1                   // number of t values in grid
+        for (i <- 1 until nx-2) u(i) = ic (i*dx)             // apply initial conditions
+        u(0) = bc._1; u(nx-1) = bc._2                        // apply boundary conditions
+
+        val mx  = nx - 2                                     // all x-values, except first and last
+        val mat = formMatrix (r, mx)                         // coefficients for next time point
+        val vec = new VectorD (mx)                           // to hold values from current time point
+
+        println ("at t = " + 0. + ": \tu = " + u)
+
+        for (j <- 1 until nt) {                              // iterative over time t = j*dt
+            vec(0)    = 2.*(1.-r)*u(1) + r*u(2) + 2.*r*bc._1
+            vec(mx-1) = r*u(mx-1) + 2.*(1.-r)*u(mx) + 2.*r*bc._2
+
+            for (i <- 1 until mx-1) {                        // iterative over position x = i*dx
+                vec(i) = r*u(i) + 2.*(1.-r)*u(i+1) + r*u(i+2)
+            } // for
+
+//          println ("mat = " + mat + "\nvec = " + vec)
+            u(1 until nx-1) = mat.solve (vec)                // solve mat * u = vec
+            println ("at t = " + j*dt + ": \tu = " + u)
+        } // for
+        u                                                    // return solution vector u
+    } // solveCN
+
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+    /** Form the tridiagonal matrix that is used in the equation "mat * u = vec".
+     *  @param r   the multiplier in the recurrence equation
+     *  @param mx  the number of positions (x) excluding the first and last
+     */
+    private def formMatrix (r: Double, mx: Int): MatrixD =
+    {
+        val mat = new MatrixD (mx, mx)
+        for (i <- 0 until mx) {
+            if (i > 0) mat(i, i-1) = -r                       // sub-digonal
+            mat(i, i) = 2.*(1.+r)                             // diagonal
+            if (i < mx-1) mat(i, i+1) = -r                    // super-diagonal
+        } // for
+        mat
+    } // formMatrix
+
+} // ParabolicPDE
+
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+/** This object is used to test the ParabolicPDE class.
+ *  Numerically solve the Heat Equation:  du/dt = k * d^2u/dx^2.
+ *  @see personales.unican.es/gutierjm/cursos/cornell/9_PDEs.pdf
+ */
+object ParabolicPDETest extends App
+{
+    val k  = 0.82                                     // thermal conductivity in cal/s*cm*0C
+    val dt = 2.                                       // delta t in sec
+    val dx = 2.5                                      // delta x in cm
+    val xm = 10.                                      // length of rod in cm
+
+    def ic (x: Double): Double = 0.                   // initial conditions
+    val bc = (100., 50.)                              // boundary conditions
+
+    val pde = new ParabolicPDE (k, dt, dx, xm, ic, bc)
+
+    println (" Explicit Finite Difference ------------------------------------------")
+    println ("solution = " + pde.solve (60.))           // solve for t = 2, 4, ... sec
+
+    println (" Implicit Crank-Nicholson --------------------------------------------")
+    println ("solution = " + pde.solveCN (60.))         // solve for t = 2, 4, ... sec
+
+} // ParabolicPDETest
+
