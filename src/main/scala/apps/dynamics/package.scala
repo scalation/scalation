bb08aabb9039584196f6889e1f642bb13fa9269b
Mustafa Nural <mvnural@gmail.com>
2016-01-06 10:00:27 -0500

    Scripted auto-commit on change (2016-01-06 10:00:27) by gitwatch.sh

--- src/main/scala/apps/dynamics/package.scala
+++
@@ -1,9 +0,0 @@
-
-package apps
-
-/** The dynamics package contains example apps for continuous
- *  simulation models based on Ordinary Differential Equations (ODEs)
- *  or Partial Differential Equations (PDEs).
- */
-package object dynamics { } 
-
--- src/main/scala/scalation/util/bld/package.scala
+++
@@ -1,8 +0,0 @@
-
-package scalation.util
-
-/** The bld package contains traits and objects for generating source code
- *  for memory mapped arrays.
- */
-package object bld { } 
-
