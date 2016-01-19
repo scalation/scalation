0ca6472c158aa240ae3fadd06b6a6a30252415ac
Mustafa Nural <mvnural@gmail.com>
2016-01-18 19:14:16 -0500

    Scripted auto-commit on change (2016-01-18 19:14:16) by gitwatch.sh

--- src/main/scala/scalation/linalgebra/bld/package.scala
+++
@@ -1,8 +0,0 @@
-
-package scalation.linalgebra
-
-/** The bld package contains traits and objects for generating source code
- *  for vector classes, matrix traits and matrix classes.
- */
-package object bld { } 
-
