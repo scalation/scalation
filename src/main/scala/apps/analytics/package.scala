b5aacfcb0a8a8b0d6e370729f4380d9ee8b9a44d
Mustafa Nural <mvnural@gmail.com>
2016-01-25 17:00:23 -0500

    Scripted auto-commit on change (2016-01-25 17:00:23) by gitwatch.sh

--- src/main/scala/apps/analytics/package.scala
+++
@@ -1,8 +0,0 @@
-
-package apps
-
-/** The analytics package contains example apps for analytics:
- *  prediction, classification or clustering.
- */
-package object analytics { } 
-
--- src/main/scala/apps/event/package.scala
+++
@@ -1,8 +0,0 @@
-
-package apps
-
-/** The event package contains example apps for event oriented
- *  simulation models.
- */
-package object event { } 
-
